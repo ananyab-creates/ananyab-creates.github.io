d645b6381fe5aece57ab48849ae5180defcddd1a
ananyab-creates <batraananyafeb8@gmail.com>
2026-01-29 21:04:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: false,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "music/Music.mp3", // Music streaming URL
+        musicUrl: "Music.mp3", // Music streaming URL
         startText: "Play Music",        // Button text to start music
         stopText: "Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
